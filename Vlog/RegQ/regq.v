4920bd07bcac42167b923ec3b0eaa6c76eeac40f
jessicah0987 <59977308+jessicah0987@users.noreply.github.com>
2020-02-28 12:51:18 -0500

    Update regq.v

--- Vlog/RegQ/regq.v
+++ Vlog/RegQ/regq.v
@@ -1,27 +1,20 @@
-
-module regq(clk,c1,c2, ia0,ia1,ia2,ia3, ib0,ib1,ib2,ib3, ic0,ic1,ic2,ic3, 
-		       id0,id1,id2,id3, q0,q1,q2,q3);
-input	clk,c1,c2,ia0,ia1,ia2,ia3, ib0,ib1,ib2,ib3, ic0,ic1,ic2,ic3,
-                      id0,id1,id2,id3;
-output	q0, q1, q2, q3;
+module regq(clk,c1,c2, ia, q);
+input	clk,c1,c2;
+input   [3:0]  ia;
+output	[3:0]	q;
 reg	[3:0]	q;
 
-//	assign {ia0,ia1,ia2,ia3} = ia;
-//	assign {ib0,ib1,ib2,ib3} = ib;
-//	assign {ic0,ic1,ic2,ic3} = ic;
-//	assign {id0,id1,id2,id3} = id;
 
-	assign {q0,q1,q2,q3} = q;
-	parameter	LD0=2'b00, LD1=2'b01, LD2=2'b10,LD3=2'b11;
 
+	parameter	Hold=2'b00, Load=2'b01, Shift=2'b10;
+	
 	always @( posedge clk)
 	begin
 
 		case({c1,c2})
-		LD0: q={ia0,ia1,ia2,ia3};
-		LD1: q={ib0,ib1,ib2,ib3};
-		LD2: q={ic0,ic1,ic2,ic3};
-		LD3: q={id0,id1,id2,id3};
+		Load: q=ia;
+		Shift: q={q[3],q}>>1;
+		Hold: ;
 		endcase
 	end
 
