a44341a2d124532565cf62214b9f618e8c7c5145
jessicah0987 <59977308+jessicah0987@users.noreply.github.com>
2020-03-06 11:28:39 -0500

    Update counter.v

--- Vlog/counter.v
+++ Vlog/counter.v
@@ -1,21 +1,28 @@
 #Creates a counter that goes high when \
 
 module counter(c, out, start);
-input	c, start;
-output	out;
-reg	[1:0] count;
+	input	c, start;
+	output	out;
+	reg	[1:0] count;
+	reg out
 
 
-always @(posedge start, c)
-begin
-    if(start == 1):
-      count = 2'b00;
-      out = 0;
-    if(c == 1):
-      count = count + 2'b01;
-	if(count == 2'b11):
-        count = 2'b00;
-        out = 1;		
-end
-
+	always @(posedge start, posedge c)
+	begin
+		if(start == 1) //resets state of register on
+			begin
+				count = 2'b00;
+				out = 0;
+			end
+		    if(c == 1)
+			begin
+				if(count == 2'b11)
+					begin
+						count = 2'b00;
+						out = 1;
+					end
+				else
+					count = count + 2'b01;
+			end
+	end
 endmodule
